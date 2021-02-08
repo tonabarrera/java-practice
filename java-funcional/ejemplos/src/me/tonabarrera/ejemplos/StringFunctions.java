ca90dda6108f4f39e144a378ac295be9dc97ee1f
Tonatihu <carlostonatihu@gmail.com>
2021-02-07 20:22:00 -0600

    Operators

--- java-funcional/ejemplos/src/me/tonabarrera/ejemplos/StringFunctions.java
+++ java-funcional/ejemplos/src/me/tonabarrera/ejemplos/StringFunctions.java
@@ -1,4 +1,16 @@
 package me.tonabarrera.ejemplos;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
 public class StringFunctions {
+    public static void main(String[] args) {
+        UnaryOperator<String> quote = text -> "\"" +text+"\"";
+        System.out.println(quote.apply("uwu"));
+
+        BiFunction<String, Integer, String> leftPath = (text, number) -> String.format("%" + number + "s", text);
+        BinaryOperator<Integer> mul = (x, y) -> x*y;
+        System.out.println(leftPath.apply("uwu", 32));
+    }
 }
